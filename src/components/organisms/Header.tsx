b8b5223a619feb7de4f6defef616394f2dccaef2
heywon0909 <hw7319@naver.com>
2023-05-15 22:10:48 +0900

    tailwind 추가

---
+++ src/components/organisms/Header.tsx
@@ -0,0 +1,8 @@
+
+const Header = () => {
+    return <header className='flex justify-between border-b border-gray-300 p-2'>
+     ticket-ing
+  </header>
+}
+
+export default Header;
---
+++ src/components/pages/Home.tsx
@@ -0,0 +1,7 @@
+const Home = () => {
+    return <div className='w-screen bg-brand-500 h-40'>
+     ticket-ing
+  </div>
+}
+
+export default Home;
